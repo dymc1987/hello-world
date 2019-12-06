94cfe3d3cd2ed24198c926e3e487bf27859b7093
mc <450316072@qq.com>
2019-12-06 17:13:59 +0800

    20191206office

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Person,Pc_assets
+
+admin.site.register(Person)
+admin.site.register(Pc_assets)
--- assets/models.py
+++ assets/models.py
@@ -4,13 +4,18 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=5,primary_key=True)
+    # 设置了name为主键，就不会创建数据表的id字段。如果这里不指定某一
+    # 字段为主键，则会自动创建id字段，作为主键
     department = models.CharField(max_length=10)
     job_position = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
 class Pc_assets(models.Model):
     person = models.ForeignKey(Person,on_delete=models.CASCADE)
     pc_type = models.CharField(max_length=20)
     sn = models.CharField(max_length=10,unique=True)
-    assets_num = models.IntegerField(max_length=15,unique=True,null=True)
+    assets_num = models.IntegerField(unique=True,null=True)
 
