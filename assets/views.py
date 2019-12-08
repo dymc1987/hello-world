bbc3559ae2f9a8e6d3085ea3d3e7271c28029ffd
mc <450316072@qq.com>
2019-12-09 00:05:02 +0800

    20191209home

--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person,Pc_assets
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, You're at the assets index.")
+
+def persons(request,person_name):
+    person = Person.objects.get(name=person_name)
+    output = f'姓名{person_name},部门{person.department},职位{person.job_position}'
+    return HttpResponse(output)
