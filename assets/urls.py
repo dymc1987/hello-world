27b8f1d0d9caf6b2cd12d3174ea228f488659d63
mc <450316072@qq.com>
2019-12-09 23:12:37 +0800

    20191209weiliu

--- assets/urls.py
+++ assets/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<person_name>/', views.persons, name='persons'),
+    path('<person_name>/', views.assets, name='assets'),
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Person,Pc_assets
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, You're at the assets index.")
+    # return HttpResponse("Hello, You're at the assets index.")
+    person = Person.objects.all()
+    context = {'name':person}
+    return render(request,'assets/index.html',context)
 
-def persons(request,person_name):
+def assets(request,person_name):
     person = Person.objects.get(name=person_name)
-    output = f'姓名{person_name},部门{person.department},职位{person.job_position}'
-    return HttpResponse(output)
+    asset = get_object_or_404(Pc_assets,person_id=person_name)
+    # context = {'asset':person.pc_assets_set.get(person_id=person_name)}
+    # # 这里注意person.pc_assets_set的使用方法
+    context = {'asset':asset}
+    return render(request,'assets/assets.html',context)
